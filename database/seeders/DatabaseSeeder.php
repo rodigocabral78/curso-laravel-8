72389422a213dfff9db869593670e6ac68970998
Rodrigo Cabral <rodrixcornell@gmail.com>
2021-08-17 19:57:04 -0400

    No ramo main
    * Mudanças a serem submetidas:
    	modified:   database/seeders/DatabaseSeeder.php
    
    @rodrixcornell Rodrigo Cabral <rodrixcornell@gmail.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        Post::factory(100)->create();
+        // Post::factory()->create();
+		$this->call([
+			PostSeeder::class,
+		]);
     }
 }
